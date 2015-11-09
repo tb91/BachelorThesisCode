9ba14bf4559afba7a4c751517e5c7ce3f88135f9
Tim Budweg <tbudweg@uni-koblenz.de>
2015-11-09 14:11:01 +0100

    save

---
+++ RNCombined/src/projects/rmys/Algorithms_ext.java
@@ -0,0 +1,59 @@
+package projects.rmys;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import projects.reactiveSpanner.Algorithms;
+import projects.reactiveSpanner.FloydWarshall.AdjMatrixEdgeWeightedDigraph;
+import projects.reactiveSpanner.FloydWarshall.DirectedEdge;
+import projects.reactiveSpanner.FloydWarshall.FloydWarshall;
+import sinalgo.nodes.Node;
+import sinalgo.runtime.nodeCollection.NodeCollectionInterface;
+import sinalgo.tools.Tools;
+
+public class Algorithms_ext {
+
+	
+	public static double rmysSpan(){
+		AdjMatrixEdgeWeightedDigraph UDGMatrix = new AdjMatrixEdgeWeightedDigraph(Tools.getNodeList().size());
+		AdjMatrixEdgeWeightedDigraph RMYSMatrix = new AdjMatrixEdgeWeightedDigraph(Tools.getNodeList().size());
+		
+		for(Node n : Tools.getNodeList()){
+			Collection<Node> neighborhood = Algorithms.getNeighborNodes(n, Tools.getNodeList());
+			Set<Node> PDTNodes = Algorithms.buildPartialDelaunayTriangulation(neighborhood,n);
+			Set<Node> RMYSNodes = buildRMYS(PDTNodes, n, Tools.getNodeList());
+			for(Node v: neighborhood)
+			{
+				DirectedEdge de = null;
+				//add edge UDG
+				// ID starts with 1, matrix with index 0
+				de = new DirectedEdge(n.ID-1, v.ID-1, n.getPosition().distanceTo(v.getPosition()));
+				UDGMatrix.addEdge(de);
+			}
+			for(Node v: RMYSNodes){
+				DirectedEdge de = null;
+				//add edge GG
+				// ID starts with 1, matrix with index 0
+				de = new DirectedEdge(n.ID-1, v.ID-1, n.getPosition().distanceTo(v.getPosition()));
+				RMYSMatrix.addEdge(de);
+
+			}
+		}
+		
+		int V = UDGMatrix.V();
+		
+		FloydWarshall UDGfw = new FloydWarshall(UDGMatrix);
+		FloydWarshall RMYSfw = new FloydWarshall(RMYSMatrix);
+		
+		return Algorithms.spanningRatio(UDGfw, RMYSfw, V);
+	}
+
+	private static <T extends Node> Set<T> buildRMYS(Collection<T> neighborhood, T n, NodeCollectionInterface nodeList) {
+		Set<T> RMYSNodes = new HashSet<T>();
+		for(T p: neighborhood){
+			
+		}
+		return null;
+	}
+}
--- RNCombined/src/projects/rmys/CustomGlobalBatch.java
+++ RNCombined/src/projects/rmys/CustomGlobalBatch.java
@@ -1,7 +1,21 @@
 package projects.rmys;
 
+import java.io.IOException;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.tools.Tool;
+
+import projects.reactiveSpanner.GraphConnectivity;
+import projects.reactiveSpanner.Utilities;
 import sinalgo.configuration.Configuration;
 import sinalgo.configuration.CorruptConfigurationEntryException;
+import sinalgo.tools.Tools;
 import sinalgo.tools.logging.LogL;
 import sinalgo.tools.logging.Logging;
 
@@ -43,22 +57,54 @@ public class CustomGlobalBatch
 
 	// for recording:
 	private static int nodeDensity;
+	private static boolean finished=false;
 	
 
 	
 	public boolean hasTerminated()
 	{
-		return true;
+		return finished;
 	}
 	
 	public void preRun()
 	{
-
+		if(GraphConnectivity.isGraphConnected(Tools.getNodeList())){
+			int numNodes = Tools.getNodeList().size();
+			nodeDensity = (int) Math.round((Math.PI * R * R / (Configuration.dimX * Configuration.dimY)) * numNodes);
+		}else{
+			logger.logln(LogL.INFO, "Graph is not connected..");
+			Tools.exit();
+		}
 	}
 
 	public void postRound()
 	{
 
+	}
+	
+	public void write_data(){
+		logger.logln(LogL.INFO, "Writing message record to file...");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+
+		final char valSep = ' ';
+		final String fileExtension = "dat";
+
+		StringBuffer line = new StringBuffer();
+		line.append(nodeDensity);
+		line.append(valSep);
+		
+		String filePathString = dateFormat.format(date) + "-record" + '.' + fileExtension;
+		OpenOption[] openOptions = new OpenOption[] { StandardOpenOption.CREATE, StandardOpenOption.APPEND };
+		List<String> lines = new ArrayList<String>();
+		lines.add(line.toString());
+
+		try {
+			Utilities.writeToFile(filePathString, lines, openOptions);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		logger.logln(LogL.INFO, "Saved message successfully to file " + filePathString);
 	}
 
 	public void onExit()
